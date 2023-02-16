6431bfa849ae07516ebef733665d84f426db4764
fauzan-radji <triputrafauzan@gmail.com>
2023-02-16 16:24:46 +0800

    dah bisa jalan

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,8 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nim')->unique();
+            $table->string('role');
+            $table->string('profile')->default('profiles/default.jpg');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -33,4 +34,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,23 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Alamat;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call([
+      AkunSeeder::class,
+      AlamatSeeder::class,
+      HargaSeeder::class
+    ]);
+    // \App\Models\User::factory(10)->create();
+  }
 }
